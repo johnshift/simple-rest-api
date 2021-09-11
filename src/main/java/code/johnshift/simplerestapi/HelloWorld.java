afd7f8633491fb683a983226e29abbd92e7ef0a6
John Ballesteros <johnshift.dev@gmail.com>
2021-09-11 17:02:55 +0800

    add cross origin for local react frontend

--- src/main/java/code/johnshift/simplerestapi/HelloWorld.java
+++ src/main/java/code/johnshift/simplerestapi/HelloWorld.java
@@ -1,10 +1,12 @@
 package code.johnshift.simplerestapi;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloWorld {
 
   @GetMapping(path = "/hello-world")
@@ -17,7 +19,7 @@ public class HelloWorld {
     return new HelloWorldBean("Hello World Bean");
   }
 
-  @GetMapping(path="/hello-world/path-variable/{name}")
+  @GetMapping(path = "/hello-world/path-variable/{name}")
   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
     return new HelloWorldBean(String.format("Hello World, %s", name));
   }
